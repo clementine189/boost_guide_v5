0eb7bb0818f6c8cf58a667f39754dd33effd1c5b
clementine189 <clementine189@163.com>
2021-02-21 11:33:41 +0800

    添加common/Makefile文件

---
+++ common/std.hpp
@@ -0,0 +1,14 @@
+#ifndef _BOOST_GUIDE_STD_HPP
+#define _BOOST_GUIDE_STD_HPP
+
+#include <cassert>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <set>
+#include <map>
+#include <algorithm>
+#include <numeric>
+
+
+#endif //_BOOST_GUIDE_STD_HPP
